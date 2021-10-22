ddd4337473ce8446a030c6875e51611412792f09
Siddhant Nivilkar <siddhantnivilkar333@gmail.com>
2021-10-22 18:39:00 +0530

    change

--- Appp.py
+++ Appp.py
@@ -1,40 +1,47 @@
-from flask import Flask, render_template, request, session
+import secrets
+from flask import Flask, render_template, request, session, flash
 from flask_mail import Mail, Message
 from flask_sqlalchemy import SQLAlchemy
 from random import randint
-import config1
+from flask_login import UserMixin, LoginManager, current_user
+from flask_admin import Admin
+
+# from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-app.config.from_envvar()
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Sidd@localhost/fyndacademy'
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-# app.config['SECRET_KEY'] = os.urandom(50)
-# app.config['MAIL_SERVER'] = "smtp.gmail.com"
-# app.config['MAIL_PORT'] = 465
-# app.config['MAIL_USE_TLS'] = False
-# app.config['MAIL_USE_SSL'] = True
-# app.config['MAIL_USERNAME'] = 'fyndproject05@gmail.com'
-# app.config['MAIL_PASSWORD'] = 'Fyndpro@05'
-# app.config['MAIL_DEFAULT_SENDER'] = 'fyndproject05@gmail.com'
-# app.config['MAIL_MAX_EMAILS'] = None
-# app.config['MAIL_SUPPRESS_SEND'] = False
-# app.config['MAIL_ASCII_ATTACHMENTS'] = False
-
+login = LoginManager()
+login.init_app(app)
+# admin = Admin(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Sidd@localhost/fyndacademy'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config['SECRET_KEY'] = secrets.token_hex(75)
+app.config['MAIL_SERVER'] = "smtp.gmail.com"
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'fyndproject05@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Fyndpro@05'
+app.config['MAIL_DEFAULT_SENDER'] = 'fyndproject05@gmail.com'
+app.config['MAIL_MAX_EMAILS'] = None
+app.config['MAIL_SUPPRESS_SEND'] = False
+app.config['MAIL_ASCII_ATTACHMENTS'] = False
 mail = Mail()
 mail.init_app(app)
 
 
 class Studloginfo(db.Model):
-    stud_name = db.Column(db.String(45), primary_key=True)
-    stud_email = db.Column(db.String(60), unique=False, nullable=False)
+    studId = db.Column(db.Integer(), primary_key=True)
+    stud_name = db.Column(db.String(45), nullable=False)
+    stud_email = db.Column(db.String(60), unique=True, nullable=False)
 
     def Js_stud(self):
         return {"stud_name": self.stud_name, "stud_email": self.stud_email}
 
 
 class Studinfo(db.Model):
-    Name = db.Column(db.String(60), primary_key=True)
+    stud_id = db.Column(db.Integer(), primary_key=True)
+    Name = db.Column(db.String(60), unique=False, nullable=False)
     Gender = db.Column(db.String(45), unique=False, nullable=False)
     Sem_1 = db.Column(db.Integer(), unique=False, nullable=False)
     Roll_no = db.Column(db.Integer(), unique=False, nullable=False)
@@ -48,13 +55,23 @@ class Studinfo(db.Model):
     Stud_Result = db.Column(db.String(45), unique=False, nullable=False)
 
     def js_re(self):
-        return {"Name": self.Name, "Gender": self.Gender,
+        return {"stud_id": self.stud_id, "Name": self.Name, "Gender": self.Gender,
                 "Sem_1": self.Sem_1, "Roll_no": self.Roll_no,
                 "Div": self.Div, "Paper_1": self.Paper_1,
                 "Paper_2": self.Paper_2, "Paper_3": self.Paper_3,
                 "Paper_4": self.Paper_4, "Paper_5": self.Paper_5,
                 "Overall_Percentage": self.Overall_Percentage,
                 "Stud_Result": self.Stud_Result}
+
+
+class Teacherlogin(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    teacher_name = db.Column(db.String(60), nullable=False)
+    teacher_email = db.Column(db.String(80), unique=True, nullable=False)
+    t_password = db.Column(db.String(60), unique=True, nullable=False)
+
+
+# admin.add_view(ModelView(Teacherlogin, db.session))
 
 
 @app.route("/")
@@ -76,6 +93,11 @@ def stafflogpa():
 @app.route("/inv_otp")
 def otp_re():
     return render_template("studD/Otppchek.html")
+
+
+@login.user_loader
+def load_user(user_id):
+    return Teacherlogin.query.get(user_id)
 
 
 def gen_otp():
@@ -107,6 +129,7 @@ def stud_info():
 
 @app.route("/Otpstud", methods=["GET", "POST"])
 def otppg():
+    global otp, otp_name, otp_email
     if request.method == "POST":
         studOTp = request.form.get("otp")
         if 'response' in session:
